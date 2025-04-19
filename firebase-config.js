b6e9f5f0543000557b3d0db06c0d60bbc464ffaf
AlejandroCastillo2406 <cesaralejandro.castillogarces24@gmail.com>
2025-04-18 19:14:19 -0600

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,22 @@
+// firebase-config.js
+// Configuración de Firebase para el sitio web de Tango's
 
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-firestore.js";
+
+// Configuración oficial extraída de Firebase Console
+const firebaseConfig = {
+  apiKey: "AIzaSybLAz-K3HpMYlbgp-ah1cdfFOEPC1YWOY8",
+  authDomain: "tangos-95f47.firebaseapp.com",
+  projectId: "tangos-95f47",
+  storageBucket: "tangos-95f47.appspot.com",
+  messagingSenderId: "855547635827",
+  appId: "1:855547635827:web:5d96d87666c8ba3ca6e206",
+  measurementId: "G-1CNC1YSLRE"
+};
+
+// Inicializar Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
